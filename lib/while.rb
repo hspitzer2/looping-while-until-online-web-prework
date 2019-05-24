ae38847dc6c0e31ad0d6bb8395e29b79b997c98a
Hillary Spitzer <hspitzer2@gmail.com>
2019-05-24 13:11:45 +0000

    Automatically backed up by Learn

--- lib/while.rb
+++ lib/while.rb
@@ -3,7 +3,7 @@ looping_string = "Wingardium Leviosa"
   levitation_force = 6
 	while  levitation_force < 10
 	  puts  "#{looping_string}"
-	  levitation_force += 1
-	  break
+	  levitation_force = levitation_force += 1
+	  break if levitation_force == 10
   end
 end
