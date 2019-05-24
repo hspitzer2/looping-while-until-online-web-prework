646c6bdd69e5530d36a5a179c5bf9a716c1828ef
Hillary Spitzer <hspitzer2@gmail.com>
2019-05-24 13:12:18 +0000

    Automatically backed up by Learn

--- lib/until.rb
+++ lib/until.rb
@@ -1,6 +1,6 @@
 def using_until
   looping_string = "Wingardium Leviosa"
-  levitation_force => 6
+  levitation_force = 6
     until levitation_force == 10
       puts "#{looping_string}"
       break if levitation_force ==10
